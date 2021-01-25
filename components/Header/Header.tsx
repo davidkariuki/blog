25ba5cff2280cf8cfc480b494657977054a095c8
David Kariukí <davidkariuki@users.noreply.github.com>
2021-01-25 16:48:24 +0000

    add mobile nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,9 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import Link from "next/link"
 
 export const Header: FC = () => {
+  const [isActive, setIsActive] = useState(true)
+
   const links = [
     {
       href: "/scribbles",
@@ -15,22 +17,57 @@ export const Header: FC = () => {
       href: "/travelogue",
       text: "travelogue",
     },
+    {
+      href: "/uses",
+      text: "uses",
+    },
   ]
 
   return (
     <div className="container p-8">
-      <nav className="flex justify-between align-middle">
+      <nav className="flex flex-wrap items-center justify-between">
         <Link href="/">
-          <div className="mr-8 text-yellow-600 text-2xl cursor-pointer font-bold">
+          <div className="mr-8 text-2xl font-bold text-yellow-700 cursor-pointer dark:text-yellow-600">
             dk
           </div>
         </Link>
-        <ul className="flex flex-row">
+        <div
+          onClick={() => setIsActive(!isActive)}
+          className="align-middle md:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            className={`${
+              isActive ? "" : "hidden"
+            } w-5 h-5 fill-current text-gray-900 dark:text-white`}
+          >
+            <path d="M2 13.5h14V12H2v1.5zm0-4h14V8H2v1.5zM2 4v1.5h14V4H2z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            className={`${
+              isActive ? "hidden" : ""
+            } w-5 h-5 fill-current text-gray-900 dark:text-white`}
+          >
+            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
+          </svg>
+        </div>
+        <ul
+          className={`${
+            isActive ? "hidden" : ""
+          } md:flex md:flex-row md:w-auto w-full border-t border-b md:border-b-0 md:border-t-0`}
+        >
           {links.map(({ href, text }) => {
             return (
-              <li className="pr-5" key={text}>
+              <li className="my-2 md:my-0 md:pr-5" key={text}>
                 <Link href={href}>
-                  <div className="inline-block align-middle mr-8 last:mr-0 cursor-pointer font-bold">
+                  <div className="mr-8 font-bold text-center align-middle border-b-2 border-transparent cursor-pointer last:mr-0 hover:border-yellow-600">
                     {text}
                   </div>
                 </Link>
