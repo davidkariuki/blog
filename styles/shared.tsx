e3255b543c42ba7677119a440135705f767aa87a
David Kariukí <davidkariuki@users.noreply.github.com>
2021-01-25 17:55:41 +0000

    update landing

--- styles/shared.tsx
+++ styles/shared.tsx
@@ -1,30 +1,4 @@
-import styled, { css } from "styled-components"
-import gh from "../public/images/github.svg"
-import tw from "../public/images/twitter.svg"
-import li from "../public/images/linkedin.svg"
-
-const svg = css`
-  fill: var(--light-text);
-  width: 1rem;
-  margin: 0 1rem;
-  height: 1rem;
-  &:hover {
-    fill: var(--accent-color);
-  }
-`
-export const GithubSvg = styled(gh)`
-  ${svg}
-`
-
-export const TwitterSvg = styled(tw)`
-  ${svg}
-`
-
-export const LinkedInSvg = styled(li)`
-  ${svg}
-`
-
-// ------------ end svg imports -----------------
+import styled from "styled-components"
 
 export const Article = styled.article`
   margin-top: 2rem;
