2fa18a2b7565afbca0305fbab182f8afb28c0962
David Kariukí <davidkariuki@users.noreply.github.com>
2021-01-31 13:20:23 +0000

    finish posts list

--- components/PostsList/PostsList.tsx
+++ components/PostsList/PostsList.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import type { PostMetadata } from "../../shared/types"
 import { FormattedDate } from "../FormattedDate"
 import { Category } from "../Category"
+import CancelFilter from "../../public/images/CancelFilter.svg"
 
 interface PostsListProps {
   posts: PostMetadata[]
@@ -10,6 +11,7 @@ interface PostsListProps {
 }
 
 export const PostsList: FC<PostsListProps> = ({ posts, categories }) => {
+  const [activeCategory, setActiveCategory] = useState<string>()
   const [postData, setPostData] = useState<PostMetadata[]>(posts)
 
   const filterPosts = (category: string): PostMetadata[] => {
@@ -18,28 +20,48 @@ export const PostsList: FC<PostsListProps> = ({ posts, categories }) => {
 
   const tagClicked = (category: string) => {
     setPostData(filterPosts(category))
+    setActiveCategory(category)
+  }
+
+  const clearCategory = () => {
+    setActiveCategory(undefined)
+    setPostData(posts)
   }
 
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row flex-wrap content-center">
+      <div className="flex flex-row flex-wrap justify-center my-4">
         {categories.map((category, index) => {
-          return <Category onClick={tagClicked} key={index} label={category} />
+          return (
+            <Category
+              className="mr-4"
+              onClick={tagClicked}
+              key={index}
+              label={category}
+              active={category === activeCategory}
+            />
+          )
         })}
+        {activeCategory && (
+          <CancelFilter
+            onClick={clearCategory}
+            className="mr-4 w-8 h-8 fill-current cursor-pointer"
+          />
+        )}
       </div>
 
       <div className="flex flex-col">
         {postData.map(({ id, title, date, description, category }) => {
           return (
             <Link key={id} href="/scribbles/[id]" as={`/scribbles/${id}`}>
-              <div className="transition duration-150 ease-in-out transform hover:scale-105 p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow cursor-pointer dark:border-dark-l dark:bg-dark-l">
-                <div>
-                  <h3>{title}</h3>
-                  <div>{description}</div>
+              <div className="flex items-center justify-between p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow cursor-pointer md:flex-row transition duration-150 ease-in-out transform hover:scale-105 dark:border-dark-l dark:bg-dark-l">
+                <div className="flex flex-col">
+                  <h3 className="text-xl">{title}</h3>
+                  <div className="text-sm">{description}</div>
+                  <FormattedDate dateString={date} className="text-sm" />
                 </div>
-                <div>
-                  <FormattedDate dateString={date} />
-                  <div>{category}</div>
+                <div className="flex flex-row mt-4 md:mt-0 md:flex-col">
+                  <Category label={category} onClick={() => {}} />
                 </div>
               </div>
             </Link>
